4208f9666d61739324ca93cd23c9a0bfab8f11bf
Dale B <daleban.dev@gmail.com>
2024-02-20 15:12:46 +0800

    style: update color theme

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -21,14 +21,19 @@ export default function Page() {
   }
 
   return (
-    <section className="container mt-20 max-w-lg">
-      <Card className="space-y-5">
+    <section className="mt-20 flex flex-col items-center justify-center gap-10">
+      <h1 className="text-7xl font-bold">
+        The Animal Institution for <br /> Lifesaving and Sanctuary
+      </h1>
+
+      <Card className="max-w-lg space-y-5">
         <CardHeader className="space-y-1">
           <CardTitle className="flex items-center justify-between text-2xl">
             <p>Login to proceed</p>
           </CardTitle>
           <CardDescription>
-            Social Media application, built with modern technologies.
+            A collaborative organization dedicated to create a centralized
+            platform for Animal Welfare Organizations.
           </CardDescription>
         </CardHeader>
 
