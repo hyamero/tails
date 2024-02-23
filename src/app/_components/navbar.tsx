e5f5d5bc93a6973bfaaeddf1697be3bff17a96f2
Dale B <daleban.dev@gmail.com>
2024-02-23 21:52:15 +0800

    chore: fix navbar color contrasts

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -59,22 +59,22 @@ export function Navbar({ sessionUser }: { sessionUser: User | null }) {
 
   return (
     <nav>
-      <div className="fixed left-0 right-0 top-0 z-50 w-full  bg-zinc-950 bg-opacity-40 bg-clip-padding py-6 backdrop-blur-xl backdrop-filter md:z-40">
+      <div className="fixed left-0 right-0 top-0 z-50 w-full bg-background py-7 backdrop-blur-xl backdrop-filter md:z-40">
         <div className="mx-auto grid w-full max-w-screen-xl grid-cols-3 items-center">
           <Link
             href="/"
             aria-label="logo button"
-            className="col-start-2 place-self-center text-4xl text-[#f3f5f7] md:col-start-1 md:ml-7 md:place-self-start"
+            className="col-start-2 place-self-center text-xl font-bold md:col-start-1 md:ml-7 md:place-self-start"
           >
-            <RiDoubleQuotesL />
+            TAILS
           </Link>
 
           <BurgerMenu user={sessionUser} />
         </div>
       </div>
 
-      <div className="fixed bottom-0 left-0 right-0 z-40 mx-auto flex max-w-screen-sm items-center justify-center gap-3 bg-zinc-950 bg-opacity-40 bg-clip-padding p-2 text-3xl backdrop-blur-xl backdrop-filter sm:px-10 md:bottom-auto md:top-0 md:z-50 md:bg-transparent md:px-14 md:text-[1.75rem] md:backdrop-blur-none [&>*:hover]:bg-zinc-900 [&>*]:flex [&>*]:w-full [&>*]:justify-center [&>*]:rounded-lg [&>*]:py-5 [&>*]:text-center [&>*]:text-zinc-700 [&>*]:transition-colors [&>*]:duration-300">
-        <Link href="/" aria-label="home button" className="hover:bg-zinc-900">
+      <div className="fixed bottom-0 left-0 right-0 z-40 mx-auto flex max-w-screen-sm items-center justify-center gap-3 bg-opacity-40 bg-clip-padding p-2 text-3xl backdrop-blur-xl backdrop-filter sm:px-10 md:bottom-auto md:top-0 md:z-50 md:bg-transparent md:px-14 md:text-[1.75rem] md:backdrop-blur-none [&>*:hover]:bg-zinc-100 [&>*]:flex [&>*]:w-full [&>*]:justify-center [&>*]:rounded-lg [&>*]:py-5 [&>*]:text-center [&>*]:text-zinc-700 [&>*]:transition-colors [&>*]:duration-300">
+        <Link href="/" aria-label="home button" className="hover:bg-zinc-100">
           <PiHouseFill className="text-center" />
         </Link>
 
