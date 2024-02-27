4f92f13b7e6a8c94c52cae7943e4c12c891b42c7
Dale B <daleban.dev@gmail.com>
2024-02-27 23:27:19 +0800

    feat: sidebar dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Feed } from "./_components/post/feed";
+import { SideBar } from "./_components/sidebar/sidebar";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center text-white">
-      <Feed />
+    <main className="container grid min-h-screen grid-cols-4 items-start pt-7 text-white">
+      <SideBar />
+      <Feed className="col-span-2 col-start-2" />
     </main>
   );
 }
