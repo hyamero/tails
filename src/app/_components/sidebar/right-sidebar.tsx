ccc2b32cd224c1719661d850451b0ef584a00745
Dale B <daleban.dev@gmail.com>
2024-02-28 19:04:32 +0800

    feat: right sidebar design

---
+++ src/app/_components/sidebar/right-sidebar.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useBoundStore } from "~/lib/use-bound-store";
+import { OrgsCard } from "./orgs-card";
+import { EventsCard } from "./events-card";
+
+export function RightSideBar({ className }: { className?: string }) {
+  const { data: session } = useSession();
+  const sessionUser = useBoundStore((state) => state.user);
+
+  if (!session || !sessionUser) return null;
+
+  return (
+    <aside
+      className={`${className} gap mx-auto flex w-full  max-w-xl flex-col space-y-2`}
+    >
+      <OrgsCard />
+      <EventsCard />
+    </aside>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Feed } from "./_components/post/feed";
 import { LeftSideBar } from "./_components/sidebar/left-sidebar";
+import { RightSideBar } from "./_components/sidebar/right-sidebar";
 
 export default async function Home() {
   return (
     <main className="grid-row-2 container flex min-h-screen grid-cols-4 flex-col items-start gap-5 pt-7 text-white lg:grid">
       <LeftSideBar />
       <Feed className="col-span-2 col-start-2" />
+      <RightSideBar className="hidden lg:block" />
     </main>
   );
 }
