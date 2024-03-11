f815dbad00af7a20aa95d040f91af6f8d40f6182
Dale B <daleban.dev@gmail.com>
2024-03-12 03:06:57 +0800

    chore: update callback

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -8,6 +8,6 @@ export const { GET, POST } = createRouteHandler({
   config: {
     uploadthingId: env.UPLOADTHING_APP_ID,
     uploadthingSecret: env.UPLOADTHING_SECRET,
-    callbackUrl: "https://tails-welfare.vercel.app/api/uploadthing/callback",
+    callbackUrl: "https://tails-welfare.vercel.app/api/uploadthing",
   },
 });
